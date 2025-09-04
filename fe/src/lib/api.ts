49158939be31f8e85e469441ae187632129b0ec3
AIS-SG <70521707+AIS-SG@users.noreply.github.com>
2025-09-04 16:52:00 +0900

    chore: be API setting

--- fe/src/lib/api.ts
+++ fe/src/lib/api.ts
@@ -4,3 +4,36 @@ export const api = axios.create({
   baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
   withCredentials: true,
 });
+
+
+export const uploadPhoto = async(file: File) => {
+  const formData = new FormData();
+  formData.append("photo", file);
+
+  try{
+    const response = await api.post("/photo/submit", formData, {headers: {"Content-Type" : "multipart/form-data",},});
+    return response.data;
+  }catch(error){
+    if(axios.isAxiosError(error)){
+      throw new Error(error.response?.data?.message || "File upload failed.");
+    }
+    throw new Error("An unexpected error occurred.");
+  }
+};
+
+export const downloadPhoto = async (fileName: string) => {
+  try {
+    const response = await api.get("/photo/download", {
+      params: {
+        name: fileName,
+      },
+      responseType: "blob",
+    });
+    return response.data;
+  } catch (error) {
+    if (axios.isAxiosError(error)) {
+      throw new Error(error.response?.data?.message || "File download failed.");
+    }
+    throw new Error("An unexpected error occurred.");
+  }
+};
